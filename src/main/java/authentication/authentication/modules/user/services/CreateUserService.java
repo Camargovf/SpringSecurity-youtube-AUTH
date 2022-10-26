84ba8b3aca8001c8167b25c7c9cea1a490a7a77a
camargo <contato@valdeircamargo.com>
2022-10-26 17:55:13 -0300

    Configurar SecurityConfig

--- src/main/java/authentication/authentication/modules/user/services/CreateUserService.java
+++ src/main/java/authentication/authentication/modules/user/services/CreateUserService.java
@@ -1,6 +1,7 @@
 package authentication.authentication.modules.user.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import authentication.authentication.modules.user.UserRepository;
@@ -9,20 +10,26 @@ import authentication.authentication.modules.user.entities.User;
 @Service
 public class CreateUserService {
 
-  @Autowired
-  UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-  public User execute(User user) {
-
-    User existsUser = userRepository.findByUsername(user.getUsername());
-
-    if (existsUser != null) {
-      throw new Error("User already exists!");
+    private BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
-    User createdUser = userRepository.save(user);
+    public User execute(User user) {
 
-    return createdUser;
-  }
+        user.setPassword(passwordEncoder().encode(user.getPassword()));
+
+        User existsUser = userRepository.findByUsername(user.getUsername());
+
+        if (existsUser != null) {
+            throw new Error("User already exists!");
+        }
+
+        User createdUser = userRepository.save(user);
+
+        return createdUser;
+    }
 
 }
---
+++ src/main/java/authentication/authentication/security/CustomUserDetails.java
@@ -0,0 +1,5 @@
+package authentication.authentication.security;
+
+public class CustomUserDetails {
+    //TODO: Implement this class 29:45
+}
